0a6a87ecb9adf96700a5770a3711da45873c313c
tmazza <tiagomdepaula@gmail.com>
2016-05-11 20:06:56 -0300

    Console application com uso de models e extensions

--- protected/config/console.php
+++ protected/config/console.php
@@ -1,4 +1,15 @@
 <?php
+require(dirname(__FILE__) . '/__config.php');
+
 return [
   'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+	'import' => [
+      'application.models.*',
+      'application.helpers.*',
+      // 'application.components.*',
+      // 'application.extensions.*',
+	],
+	'components' => [
+		'db' => require(dirname(__FILE__) . '/database.php'),
+	]
 ];
