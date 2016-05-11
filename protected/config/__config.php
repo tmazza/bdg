b43b3fb10dd4fc125480cfc714eb177363ff44ca
tmazza <tiagomdepaula@gmail.com>
2016-05-11 19:46:18 -0300

    Arquivo de senhas da aplicação

---
+++ protected/config/__config.php
@@ -0,0 +1,22 @@
+<?php
+# Conexão com DB
+$__hostDatabase = '';
+$__nameDatabase = '';
+$__userDatabase = '';
+$__passDatabase = '';
+$__portDatabase = ;
+
+#chaves APIs
+$__awsSmtpHost = '';
+$__awsUsername = '';
+$__awsPassword = '';
+$__faceID = '';
+$__faceSecret = '';
+$__googleId = '';
+$__googleSecret = '';
+
+# Parâmetros auxiliares
+$__emailDeNotificacao = '';
+$__dominio = 'localhost:8080'; # Usado para geração de links nos emails
+$__s3BucketId = '?';
+$__s3BucketEndPoint = '?';
