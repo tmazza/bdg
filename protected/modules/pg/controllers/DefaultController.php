ca5fcf0685b7897b337be8fb13e35b8be9f90199
tmazza <tiagomdepaula@gmail.com>
2016-05-13 22:01:16 -0300

    Testes fluxo de geração de pagamento

--- protected/modules/pg/controllers/DefaultController.php
+++ protected/modules/pg/controllers/DefaultController.php
@@ -7,17 +7,10 @@
 class DefaultController extends PgController {
 
     public function actionIndex() {
-      $bolao = Bolao::model()->find();
-
-      $url = PaymentRequestGenerator::pagamentoBolao($bolao);
-      if($url){
-        echo $url;
-        exit;
-        $this->redirect($url);
-      } else {
-        HView::ferr("Ops. Não foi possível gerar a solicitação de pagamento. Tente mais um vez.");
-      }
-      $this->render('index', []);
+      $boloes = Bolao::model()->findAll();
+      $this->render('index', [
+        'boloes' => $boloes,
+      ]);
     }
 
     public function actionListener(){
--- protected/modules/pg/views/default/index.php
+++ protected/modules/pg/views/default/index.php
@@ -1,1 +1,7 @@
-\o/
+<?php foreach ($boloes as $b) {
+  echo CHtml::link($b->nome,$this->createUrl("/pg/comprar/bolao",[
+    'id'=>$b->id,
+  ]),[
+    'onclick' => '$(this).html("<i class=\"uk-icon uk-icon-spin uk-icon-spinner\"></i> Redirecionando para o PagSeguro. Aguarde...")',
+  ]) . '&nbsp;&nbsp;&nbsp;&nbsp;R$ ' . number_format($b->inscricao,2,',','.') . '<br>';
+}
