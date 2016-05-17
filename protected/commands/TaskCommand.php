a9d191b5d0653dfc9b7db9352eb287626d838789
tmazza <tiagomdepaula@gmail.com>
2016-05-17 13:39:12 -0300

    Criação de tabela de pendencias/alteracoes salva ao identificar atualizacoes

--- protected/commands/TaskCommand.php
+++ protected/commands/TaskCommand.php
@@ -24,13 +24,13 @@ class TaskCommand extends CConsoleCommand
     }
 
     private function atualizacoesBrasileiroA2016(){
-      $id = 'UPDATES_BRASILEIRO_A_2016';
+      $id = 'BRA16';
       $lastHash = $this->getLastPage($id);
       $html = $this->parserHTML->file_get_html('http://www.cbf.com.br/competicoes/brasileiro-serie-a/alteracoes-de-jogos/2016');
       $hash = hash('sha512',$html->plaintext);
 
       if($lastHash == $hash){
-        $this->saveLog("UP_BRASILEIRO_A_2016 Sem alterações");
+        $this->saveLog("{$id} Sem alterações");
       } else {
         $divs = $html->find('div');
         $divsAlts = [];
@@ -39,30 +39,65 @@ class TaskCommand extends CConsoleCommand
             $divsAlts[] = $d;
           }
         }
-        $alteacoes = [];
+
+        $pagesHash = $this->getHashPages($id);
+
+        $alteracoes = [];
         foreach ($divsAlts as $d) {
-          # Busca numero do jogo
-          $blocoNumJogo = $d->find('.small',0)->innertext;
-          $numeros = [];
-          preg_match_all('/[0-9]{1,3}/',$blocoNumJogo,$numeros);
-          $numJogo = $numeros[0][0];
+          $hashPage = hash('sha512',$d->innertext);
+          if(!in_array($hashPage,$pagesHash)){
+            $pagesHash[] = $hashPage;
+            # Busca numero do jogo
+            $titulo = $d->find('h4',0)->innertext;
+            $blocoNumJogo = $d->find('.small',0)->innertext;
+            $numeros = [];
+            preg_match_all('/[0-9]{1,3}/',$blocoNumJogo,$numeros);
+            $numJogo = $numeros[0][0];
 
-          # Busca nova data e novo horário
-          $table = $d->find('table',0);
-          $data = $table->find('tr',2)->find('td',2)->plaintext;
-          $hora = $table->find('tr',3)->find('td',2)->plaintext;
+            # Busca nova data e novo horário
+            $table = $d->find('table',0);
+            $dataDe = $table->find('tr',2)->find('td',1)->plaintext;
+            $horaDe = $table->find('tr',3)->find('td',1)->plaintext;
+            $dataPara = $table->find('tr',2)->find('td',2)->plaintext;
+            $horaPara = $table->find('tr',3)->find('td',2)->plaintext;
 
-          $alteacoes[] = [
-            'jogo'=>$numJogo,
-            'data'=>$data,
-            'hora'=>$hora,
-          ];
+            $motivo = $table->find('tr',5)->find('td',1)->plaintext;
+
+            $alteracoes[] = [
+              'codCampeonato'=>$id,
+              'descricao'=>$titulo,
+              'numJogo'=>$numJogo,
+              'de'=>$dataDe . ' - ' . $horaDe,
+              'para'=>$dataPara . ' - ' . $horaPara,
+              'motivo'=>$motivo,
+            ];
+          }
         }
-        # salva como já realizada!
-        $this->hashAntigo[$id] = $hash;
-        $this->setLastPage();
-        $this->saveLog("UP_BRASILEIRO_A_2016 Encontradas " . count($alteacoes) . " posíveis alterações");
+        if($this->registraPendencia($alteracoes)){
+          # salva como já realizada!
+          $this->hashAntigo[$id] = [
+            'hash'=>$hash,
+            'paginas'=>$pagesHash,
+          ];
+          $this->setLastPage();
+          $msg = "{$id} Encontradas " . count($alteracoes) . " posíveis alterações";
+          $this->saveLog($msg);
+          HEmail::toAdminNoTemplate("Atualizações aguardando avaliação.",$msg . (count($alteracoes)==0?'**Somente pág. alterada.Sem Alterações nos jogos.':''));
+        } else {
+          $this->saveLog('ERRO AO SALVAR ALTERACOES: ' . $msg);
+        }
       }
+    }
+
+    private function registraPendencia($alteracoes){
+      $ok = true;
+      foreach ($alteracoes as $a) {
+        $model = new Alteracao();
+        $model->data = time();
+        $model->attributes = $a;
+        $ok = $model->save() && $ok;
+      }
+      return $ok;
     }
 
     /**
@@ -84,14 +119,27 @@ class TaskCommand extends CConsoleCommand
         $this->hashAntigo = json_decode(fread($handle,filesize($this->filename)),true);
         fclose($handle);
       }
-      return isset($this->hashAntigo[$id]) ? $this->hashAntigo[$id] : false;
+      return isset($this->hashAntigo[$id]) ? $this->hashAntigo[$id]['hash'] : false;
     }
+
+    /**
+     * Retorna os hashs das páginas/blocos já processado.
+     */
+    private function getHashPages($id){
+      if(!$this->hashAntigo && file_exists($this->filename)){
+        $handle = fopen($this->filename,'r');
+        $this->hashAntigo = json_decode(fread($handle,filesize($this->filename)),true);
+        fclose($handle);
+      }
+      return isset($this->hashAntigo[$id]) ? $this->hashAntigo[$id]['paginas'] : [];
+    }
+
 
     /**
      * Grava lista de hash já analisados
      */
     private function setLastPage(){
-      $handle = fopen($this->filename,'a+');
+      $handle = fopen($this->filename,'w+');
       fwrite($handle,json_encode($this->hashAntigo));
       fclose($handle);
     }
--- protected/data/estrutura.sql
+++ protected/data/estrutura.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 17, 2016 at 03:06 PM
+-- Generation Time: May 17, 2016 at 04:38 PM
 -- Server version: 5.5.43-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.11
 
@@ -19,6 +19,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `bdg`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alteracao`
+--
+
+CREATE TABLE IF NOT EXISTS `alteracao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codCampeonato` varchar(5) NOT NULL,
+  `descricao` text,
+  `numJogo` int(11) NOT NULL,
+  `de` text NOT NULL,
+  `para` text NOT NULL,
+  `motivo` text,
+  `data` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
 
 -- --------------------------------------------------------
 
