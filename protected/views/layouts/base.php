a0db5805ca6e851d2bca3abf6718ab4904aada9c
tmazza <tiagomdepaula@gmail.com>
2016-05-25 14:12:09 -0300

    Analytics add

--- protected/views/layouts/base.php
+++ protected/views/layouts/base.php
@@ -2,6 +2,18 @@
 <html lang='pt' style="background:#dedede;">
     <?php $this->renderPartial('//layouts/head'); ?>
     <body>
+			<?php if(!YII_DEBUG): ?>
+			<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-31889911-1', 'auto');
+			ga('send', 'pageview');
+			</script>
+			<?php endif; ?>
+
       <div id='header'>
         <div class="uk-container uk-container-center">
           <a href="<?=$this->createUrl('/site/index')?>" class="" style="padding:10px 2px;">
