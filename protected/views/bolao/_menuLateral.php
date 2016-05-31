ca9f39117c89b1ae45951b9645001592d80f813d
Your Name <tiagomdepaula@gmail.com>
2016-05-31 04:03:23 +0000

    Propaganda lateral

--- protected/views/bolao/_menuLateral.php
+++ protected/views/bolao/_menuLateral.php
@@ -4,7 +4,16 @@
     'id'=>$bolao->idBolao,
   ]),HView::modalUpdate('main-modal-large'))?></li>
 </ul>
-
+<br>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Lateral -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:600px"
+     data-ad-client="ca-pub-3847082477827226"
+     data-ad-slot="7794195282"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 <?php
 // echo CHtml::ajaxLink('Ver tabela do Brasileirão',$this->createUrl('/bolao/loadTabBra'),[
 //   'success'=>'js:function(html){ $("#ver-tabela").html(html); }',
