9d0f5fa3b6ae48348490d7f9a8780c2fadf2a4e0
tmazza <tiagomdepaula@gmail.com>
2016-07-18 18:06:32 -0300

    Fix página configurações

--- protected/controllers/ConfiguracaoController.php
+++ protected/controllers/ConfiguracaoController.php
@@ -3,29 +3,33 @@
 class ConfiguracaoController extends MainController {
 
   public function actionIndex(){
+    if($this->user){
+      $model = new Configuracao();
+      $model->emailFechaDia = $this->user->emailFechaDia;
+      $model->emailAvisoFechaDia = $this->user->emailAvisoFechaDia;
+      $model->faceAvisoFechaDia = $this->user->faceAvisoFechaDia;
 
-    $model = new Configuracao();
-    $model->emailFechaDia = $this->user->emailFechaDia;
-    $model->emailAvisoFechaDia = $this->user->emailAvisoFechaDia;
-    $model->faceAvisoFechaDia = $this->user->faceAvisoFechaDia;
-
-    if(isset($_POST['Configuracao'])){ 
-      $model->attributes = $_POST['Configuracao'];
-      if($model->validate()){
-        $this->user->emailFechaDia = $model->emailFechaDia;
-        $this->user->emailAvisoFechaDia = $model->emailAvisoFechaDia;
-        $this->user->faceAvisoFechaDia = $model->faceAvisoFechaDia;
-        if($this->user->update(['emailFechaDia','emailAvisoFechaDia','faceAvisoFechaDia']) > 0){
-          HView::fsuc("Alterações salvas.");          
-        } else {
-          HView::finf("Nada modificado.");          
+      if(isset($_POST['Configuracao'])){ 
+        $model->attributes = $_POST['Configuracao'];
+        if($model->validate()){
+          $this->user->emailFechaDia = $model->emailFechaDia;
+          $this->user->emailAvisoFechaDia = $model->emailAvisoFechaDia;
+          $this->user->faceAvisoFechaDia = $model->faceAvisoFechaDia;
+          
+          if($this->user->update(['emailFechaDia','emailAvisoFechaDia','faceAvisoFechaDia']) > 0){
+            HView::fsuc("Alterações salvas.");          
+          } else {
+            HView::finf("Nada modificado.");          
+          }
         }
       }
-    }
 
-    $this->render('index',[
-      'model'=>$model,
-    ]);
+      $this->render('index',[
+        'model'=>$model,
+      ]);
+    } else {
+      $this->redirect($this->createUrl('/site/index'));
+    }
   }
 
 }
