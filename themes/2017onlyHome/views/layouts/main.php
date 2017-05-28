5775580b77aa27874394cd6c2bac8e37ccb3f692
tmazza <tiagomdepaula@gmail.com>
2017-05-28 19:24:21 -0300

    Base página inicial

--- themes/2017onlyHome/views/layouts/main.php
+++ themes/2017onlyHome/views/layouts/main.php
@@ -38,31 +38,45 @@
 
     <!-- Banner -->
       <section id="banner">
-        <h2>Reunindo fãs do futebol desde 2007</h2>
-        <p>Ganhe prêmios e se divirta junto com amigos e familiares</p>
+        <h2 style="font-size: 30px;">Reunindo fãs do futebol desde 2007</h2>
+        <p>Ganhe prêmios e divirta-se junto com amigos e familiares</p>
         <ul class="actions">
           <li>
             <a href="#conheca" class="button big">Conheça</a>
+            <a href="<?=$this->createUrl('/cadastro/index');?>" class="button big" style='background: transparent;'>
+              Cadastre-se
+            </a>
           </li>
         </ul>
       </section>
 
     <!-- One -->
+
+      <section id="conheca" class="wrapper style1 special">
+        <div class="container">
+          <header class="major">
+            <h2>Bolão 2017</h2>
+            <p>Neste ano teremos o bolão pago do Brasileirão de 2017 série A. <!-- O valor da inscrição é R$ 50,00. --></p>
+          </header>
+        </div>
+      </section>
+
+    <!--
       <section id="conheca" class="wrapper style1 special">
         <div class="container">
           <header class="major">
             <h2>Como funciona</h2>
             <p>Modalidade paga e modalidade gratuita. Como assim?</p>
           </header>
-          <div class="row 150%">
-            <div class="6u 12u$(medium)">
+          <div class="row ">
+            <div class="6u 6u$(medium) 12u$(small)">
               <section class="box">
                 <i class="icon big rounded color1 fa-cloud"></i>
                 <h3>Lorem ipsum dolor</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim quam consectetur quibusdam magni minus aut modi aliquid.</p>
               </section>
             </div>
-            <div class="6u$ 12u$(medium)">
+            <div class="6u 6u$(medium) 12u$(small)">
               <section class="box">
                 <i class="icon big rounded color6 fa-rocket"></i>
                 <h3>Adipisicing elit totam</h3>
@@ -73,7 +87,6 @@
         </div>
       </section>
 
-    <!-- Two -->
       <section id="two" class="wrapper style2 special">
         <div class="container">
           <header class="major">
@@ -83,22 +96,22 @@
           <section class="profiles">
             <div class="row">
               <section class="3u 6u(medium) 12u$(xsmall) profile">
-                <img src="<?=$this->assetsDir;?>/2017onlyHome/images/profile_placeholder.gif" alt="" />
+                <img src="<?//=$this->assetsDir;?>/2017onlyHome/images/profile_placeholder.gif" alt="" />
                 <h4>Lorem ipsum</h4>
                 <p>Lorem ipsum dolor</p>
               </section>
               <section class="3u 6u$(medium) 12u$(xsmall) profile">
-                <img src="<?=$this->assetsDir;?>/2017onlyHome/images/profile_placeholder.gif" alt="" />
+                <img src="<?//=$this->assetsDir;?>/2017onlyHome/images/profile_placeholder.gif" alt="" />
                 <h4>Voluptatem dolores</h4>
                 <p>Ullam nihil repudi</p>
               </section>
               <section class="3u 6u(medium) 12u$(xsmall) profile">
-                <img src="<?=$this->assetsDir;?>/2017onlyHome/images/profile_placeholder.gif" alt="" />
+                <img src="<?//=$this->assetsDir;?>/2017onlyHome/images/profile_placeholder.gif" alt="" />
                 <h4>Doloremque quo</h4>
                 <p>Harum corrupti quia</p>
               </section>
               <section class="3u$ 6u$(medium) 12u$(xsmall) profile">
-                <img src="<?=$this->assetsDir;?>/2017onlyHome/images/profile_placeholder.gif" alt="" />
+                <img src="<?//=$this->assetsDir;?>/2017onlyHome/images/profile_placeholder.gif" alt="" />
                 <h4>Voluptatem dicta</h4>
                 <p>Et natus sapiente</p>
               </section>
@@ -109,87 +122,30 @@
             Explicar que existe um periodo em que é possível participar do bolão pago sem precisar pagar  ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolore illum, temporibus veritatis eligendi, aliquam, dolor enim itaque veniam aut eaque sequi qui quia vitae pariatur repudiandae ab dignissimos ex!</p>
             <ul class="actions">
               <li>
-                <a href="<?=$this->createUrl('/cadastro/index');?>" class="button big">Comece agora</a>
+                <a href="<?//=$this->createUrl('/cadastro/index');?>" class="button big">Comece agora</a>
               </li>
             </ul>
           </footer>
         </div>
       </section>
-
-    <!-- Three -->
-      <section id="three" class="wrapper style3 special">
-        <div class="container">
-          <header class="major">
-            <h2>Consectetur adipisicing elit</h2>
-            <p>Lorem ipsum dolor sit amet. Delectus consequatur, similique quia!</p>
-          </header>
-        </div>
-        <div class="container 50%">
-          <form action="#" method="post">
-            <div class="row uniform">
-              <div class="6u 12u$(small)">
-                <input name="name" id="name" value="" placeholder="Name" type="text">
-              </div>
-              <div class="6u$ 12u$(small)">
-                <input name="email" id="email" value="" placeholder="Email" type="email">
-              </div>
-              <div class="12u$">
-                <textarea name="message" id="message" placeholder="Message" rows="6"></textarea>
-              </div>
-              <div class="12u$">
-                <ul class="actions">
-                  <li><input value="Send Message" class="special big" type="submit"></li>
-                </ul>
-              </div>
-            </div>
-          </form>
-        </div>
-      </section>
-
+    -->
     <!-- Footer -->
       <footer id="footer">
         <div class="container">
           <section class="links">
             <div class="row">
               <section class="3u 6u(medium) 12u$(small)">
-                <h3>Lorem ipsum dolor</h3>
-                <ul class="unstyled">
-                  <li><a href="#">Lorem ipsum dolor sit</a></li>
-                  <li><a href="#">Nesciunt itaque, alias possimus</a></li>
-                  <li><a href="#">Optio rerum beatae autem</a></li>
-                  <li><a href="#">Nostrum nemo dolorum facilis</a></li>
-                  <li><a href="#">Quo fugit dolor totam</a></li>
+                <ul class="">
+                  <li><?=CHtml::link("<i class='uk-icon uk-icon-soccer-ball-o'></i> Regulamento geral",$this->createUrl('/regulamento/geral'),['class'=>'uk-button uk-button-link'])?></li>
+                  <li><?=CHtml::link("Sobre o PagSeguro",'https://pagseguro.uol.com.br/para_voce/como_funciona.jhtml',['class'=>'uk-button uk-button-link'])?></li>
+                  <li><?=CHtml::link("<i class='uk-icon uk-icon-bug'></i> Reportar um erro",'https://github.com/tmazza/BdG/issues/new',['class'=>'uk-button uk-button-link'])?></li>
                 </ul>
               </section>
               <section class="3u 6u$(medium) 12u$(small)">
-                <h3>Culpa quia, nesciunt</h3>
-                <ul class="unstyled">
-                  <li><a href="#">Lorem ipsum dolor sit</a></li>
-                  <li><a href="#">Reiciendis dicta laboriosam enim</a></li>
-                  <li><a href="#">Corporis, non aut rerum</a></li>
-                  <li><a href="#">Laboriosam nulla voluptas, harum</a></li>
-                  <li><a href="#">Facere eligendi, inventore dolor</a></li>
-                </ul>
               </section>
               <section class="3u 6u(medium) 12u$(small)">
-                <h3>Neque, dolore, facere</h3>
-                <ul class="unstyled">
-                  <li><a href="#">Lorem ipsum dolor sit</a></li>
-                  <li><a href="#">Distinctio, inventore quidem nesciunt</a></li>
-                  <li><a href="#">Explicabo inventore itaque autem</a></li>
-                  <li><a href="#">Aperiam harum, sint quibusdam</a></li>
-                  <li><a href="#">Labore excepturi assumenda</a></li>
-                </ul>
               </section>
               <section class="3u$ 6u$(medium) 12u$(small)">
-                <h3>Illum, tempori, saepe</h3>
-                <ul class="unstyled">
-                  <li><a href="#">Lorem ipsum dolor sit</a></li>
-                  <li><a href="#">Recusandae, culpa necessita nam</a></li>
-                  <li><a href="#">Cupiditate, debitis adipisci blandi</a></li>
-                  <li><a href="#">Tempore nam, enim quia</a></li>
-                  <li><a href="#">Explicabo molestiae dolor labore</a></li>
-                </ul>
               </section>
             </div>
           </section>
@@ -203,22 +159,22 @@
             <div class="4u$ 12u$(medium)">
               <ul class="icons">
                 <li>
-                  <a class="icon rounded fa-facebook"><span class="label">Facebook</span></a>
+                    <a href="https://www.facebook.com/bolaodogordo" target="_blank" class="icon rounded fa-facebook" data-uk-tooltip title="Facebook"><span class="label">Facebook</span></a>
                 </li>
                 <li>
-                  <a class="icon rounded fa-twitter"><span class="label">Twitter</span></a>
+                    <a href="https://twitter.com/bolaodogordo" target="_blank" class="icon rounded fa-twitter" data-uk-tooltip title="Twitter"><span class="label">Twitter</span></a>
                 </li>
                 <li>
-                  <a class="icon rounded fa-google-plus"><span class="label">Google+</span></a>
-                </li>
-                <li>
-                  <a class="icon rounded fa-linkedin"><span class="label">LinkedIn</span></a>
+                    <a href="https://github.com/tmazza/bdg" target="_blank" class="icon rounded fa-github" data-uk-tooltip title="GitHub"><span class="label">Github</span></a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
       </footer>
+
+
+
       <?php if(!YII_DEBUG): ?>
       <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
