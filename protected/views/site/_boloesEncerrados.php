91fecbc347a8159fa9b1678547fcb2131297ab37
tmazza <tiagomdepaula@gmail.com>
2017-05-31 20:34:35 -0300

    Bolões encerrados em listas

--- protected/views/site/_boloesEncerrados.php
+++ protected/views/site/_boloesEncerrados.php
@@ -1,20 +1,13 @@
   <br>
   <div class="uk-panel uk-panel-box uk-panel-box-secondary">
     <h3>Bolões encerrados</h3>
-    <div class="uk-grid">
+    <ul class="">
       <?php foreach ($boloesEncerrados as $b): ?>
-          <div class="uk-width-medium-1-3 uk-width-large-1-4 uk-width-small-1-2">
+          <li>
             <a href='<?=$this->createUrl('/bolao/index',['id'=>$b->idBolao]);?>'>
-              <div class="uk-thumbnail uk-thumbnail-expand">
-                  <?php if(!is_null($b->capa)): ?>
-                    <img src="<?=$b->capa;?>" alt="Imagem <?=$b->nome?>">
-                  <?php endif; ?>
-                  <div class="uk-thumbnail-caption">
-                    <h3><?=$b->nome;?></h3>
-                  </div>
-              </div>
+              <?=$b->nome;?>
             </a>
-          </div>
+          </li>
       <?php endforeach; ?>
-    </div>
+    </ul>
   </div>
