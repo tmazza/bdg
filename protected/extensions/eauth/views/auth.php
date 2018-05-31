483503d2c281b88282bf5cee265712cd3d3820f8
tmazza <tiagomdepaula@gmail.com>
2018-05-31 19:16:43 -0300

    Ajuste botão social

--- protected/extensions/eauth/views/auth.php
+++ protected/extensions/eauth/views/auth.php
@@ -1,7 +1,7 @@
 <?php foreach ($services as $name => $service): ?>
 	<?php if($service->id == 'facebook'): ?>
-		<div class="uk-panel" style="margin-bottom:-10px;">
-			<div style="display:block;width:230px;background:#3B5998;" class="uk-button auth-service <?=$service->id;?>">
+		<div style="height: 50px;">
+			<div style="display:block;width:290px;background:#3B5998;" class="uk-button auth-service <?=$service->id;?>">
 				<?php
 				$html = '<i class="uk-icon-facebook-official" style="font-size:32px;padding:4px;float:left;"></i>';
 				$html .= '<div style="padding-top:8px;">&nbsp;&nbsp;Entrar com ' . $service->title . '</div>';
@@ -12,22 +12,22 @@
 
 				$html = CHtml::link($html, $url, array(
 					'class' => 'auth-link ' . $service->id,
-					'style' => 'display:block;color:white;text-decoration:none;text-align:left;padding-bottom:5px;'
+					'style' => 'display:block;color:white;text-shadow:none;font-weight:bold;text-decoration:none;text-align:left;padding-bottom:5px;'
 				));
 				echo $html;
 				?>
 			</div>
-		</div><br>
+		</div>
 	<?php else: ?>
-		<div class="uk-panel">
-			<div style="display:block;width:230px;background:#DD4B39;" class="uk-button auth-service <?=$service->id;?>">
+		<div style="height: 50px;">
+			<div style="display:block;width:290px;background:#DD4B39;" class="uk-button auth-service <?=$service->id;?>">
 			<?php
 			$html = '<i class="uk-icon-google" style="font-size:32px;padding:4px;float:left;"></i>';
 			$html .= '<div style="padding-top:8px;">&nbsp;&nbsp;&nbsp;Entrar com ' . $service->title . '</div>';
 			$url = Yii::app()->controller->createUrl($action,array('service' => $name)) . (isset($_GET['t'])?$_GET['t']:'');
 			$html = CHtml::link($html,$url, array(
 				'class' => 'auth-link ' . $service->id,
-				'style' => 'display:block;color:white;text-decoration:none;text-align:left;padding-bottom:5px;'
+				'style' => 'display:block;color:white;text-shadow:none;font-weight:bold;text-decoration:none;text-align:left;padding-bottom:5px;'
 			));
 			echo $html;
 			?>
